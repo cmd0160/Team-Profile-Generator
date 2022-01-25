908c2b437335b67d1b714aa51965670db0344d9c
cdtest <corydavis1989@gmail.com>
2022-01-24 23:31:05 -0600

    Employee test Passed

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -2,13 +2,16 @@ const Employee = require('../lib/Employee');
 
 
 test('creates an Employee object', ()=>{
-    const employee = new Employee('Dave','12345 ','12345@gmail.com');
+    const employee = new Employee('Dave', 12345 ,'12345@gmail.com');
 
-    expect(employee.name).toEqual(String);
-    expect(employee.Id).toEqual(expect.any(Number));
-    expect(employee.Email).toEqual(expect.any(String));
+    expect(employee.name).toBe('Dave');
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
     expect(employee.getName()).toEqual(employee.name);
-    expect(employee.getId()).toEqual(employee.Id);
-    expect(employee.getEmail()).toEqual(employee.Email);
+    expect(employee.getId()).toEqual(employee.id);
+    expect(employee.getEmail()).toEqual(employee.email);
     expect(employee.getRole()).toEqual(employee.role);
 });
+
+
+
